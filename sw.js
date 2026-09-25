8df6706ad97750fc2daa0b77b76763a63425d12b
richard300348 <richard300348@gmail.com>
2026-09-24 23:38:36 -0500

    Cache Carmen audio for offline playback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME='retro-hub-v1';
-const APP_SHELL=['./','./index.html','./style.css','./app.js','./manifest.json'];
+const APP_SHELL=['./','./index.html','./style.css','./app.js','./manifest.json','./media/carmen-prelude.ogg','./media/carmen-toreador.ogg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
